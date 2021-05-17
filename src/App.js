d18169a4ed72e7c60122bf58b61bb5ca47871d70
Laurenz Grote <laurenzfg@laurenzgrote.de>
2021-05-17 15:59:54 +0200

    Statische Inhalte

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="App">
+        <p>Am 01.01.2021 wurden in Deutschland 0 Impfdosen verabreicht.
+        Davon entfielen 0 auf Erst- und 0 auf Zweitimpfungen.</p>
+
+        <p>Über eine mindestens einmalige Impfung verfügen aktuell 0 Menschen.
+        Das sind 0 % der Einwohner:innen Deutschlands.</p>
+
+        <p>In den Genuss eines vollständigen Impfschutzes kommen aktuell 0 Menschen.
+        Das sind 0 % der Einwohner:innen Deutschlands.</p>
+
+        <p>Der Impfstoffmix aller begonnenn und abgeschlossenen Impfserien ist:
+          0 Biontech, 0 Moderna, 0 AstraZeneca und 0 Janssen.</p>
+      </div>
+      <div className="info">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Wenn Du die neuen Impfdaten auch nicht erwarten kannst, kannst du mit der
+          Glocke Push-Nachrichten abonnieren. Wir senden Dir dann innert 30 Minuten nach
+          Veröffentlichung durch das RKI eine Push Nachricht zu.
+          Leider können wir diesen Service auf Apple-Geräten nicht bieten.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      <footer>
+        <p>Engineered with ❤️ in Aachen.</p>
+      </footer>
+    </>
   );
 }
 
