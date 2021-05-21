c2e73aa609f42050965de067ed1797f3d9a40885
Laurenz Grote <laurenzfg@laurenzgrote.de>
2021-05-21 11:04:59 +0200

    Easier data schema

--- src/App.js
+++ src/App.js
@@ -9,45 +9,52 @@ function App() {
 
   const [vacData, setVacData] = useState({
       administeredVaccinations: 0,
-      vaccinated: 0,
-      vaccination: {
+      vaccinated: 0, // at least first vaccination
+      vaccination: { // vaccination series according to first jab
         biontech: 0,
         moderna: 0,
         astraZeneca: 0,
+        janssen: 0,
+        curevac: 0
       },
-      delta: 0,
-      quote: 0,
+      delta: 0, // first vaccinations yesterday
+      quote: 0, // percentage vaccinated at least one
       secondVaccination: {
-        vaccinated: 0,
-        vaccination: {
-          biontech: 0,
-          moderna: 0,
-          astraZeneca: 0,
-          janssen: 0,
-        },
-        delta: 0,
-        quote: 0,
-      },
-      indication: {
-        age: null,
-        job: null,
-        medical: null,
-        nursingHome: null,
-        secondVaccination: {
-          age: null,
-          job: null,
-          medical: null,
-          nursingHome: null,
-        },
-      },
-      states: {},
+        vaccinated: 0, // fully vaccinated; everybody who had two doses or one janssen
+        delta: 0, // seccond jabs + janssen jabs yesterday
+        quote: 0, // percentage fully vaccination
+      }
   });
 
   useEffect(() => {
     // Update the document title using the browser API
     axios.get(apiEndpoint)
     .then((response) => {
-        setVacData(response.data.data);
+        const apiData = response.data.data;
+        
+        // Interesting Quirk: RKI / API does not count Janssen as a first dose, they count it second dose ONLY
+        // Thus do get "at least started vaccination series" in our sense, we need to add "started series" in their sense,
+        // which is first shots of two dose vaccines, to the one-shot janssen vaccinations!
+
+        setVacData({
+          administeredVaccinations: apiData.administeredVaccinations,
+          vaccinated: apiData.vaccinated + apiData.secondVaccination.vaccination.janssen,
+          vaccination: {
+            biontech: apiData.vaccination.biontech,
+            moderna: apiData.vaccination.moderna,
+            astraZeneca: apiData.vaccination.astraZeneca,
+            janssen: apiData.secondVaccination.vaccination.janssen,
+            curevac: 0
+          },
+          delta: apiData.delta,
+          quote: apiData.quote,
+          secondVaccination: {
+            vaccinated: apiData.secondVaccination.vaccinated,
+            delta: apiData.secondVaccination.delta,
+            quote: apiData.secondVaccination.quote,
+          }
+        });
+
         let lastUp = new Date(response.data.meta.lastUpdate);
         lastUp.setTime(lastUp.getTime()-(24*60*60*1000));
         setLu(lastUp);
@@ -76,8 +83,9 @@ function App() {
         <p>In den Genuss eines vollständigen Impfschutzes kommen aktuell {fancynum(vacData.secondVaccination.vaccinated)} Menschen.
         Das sind {percentage(vacData.secondVaccination.quote)} % der Einwohner:innen Deutschlands.</p>
 
-        <p>Der Impfstoffmix aller begonnenen und abgeschlossenen Impfserien ist: {fancynum(vacData.vaccination.biontech)} Biontech,&nbsp;
-        {fancynum(vacData.vaccination.moderna)} Moderna, {fancynum(vacData.vaccination.astraZeneca)} AstraZeneca und {fancynum(vacData.secondVaccination.vaccination.janssen)} Janssen.</p>
+        <p>Die Impfserien bzw. One-Shot-Impfungen wurden jeweils mit folgenden Impfstoffen begonnen: {fancynum(vacData.vaccination.biontech)} Biontech,&nbsp;
+        {fancynum(vacData.vaccination.moderna)} Moderna, {fancynum(vacData.vaccination.astraZeneca)} AstraZeneca und {fancynum(vacData.vaccination.janssen)} Janssen.</p>
+        <p>Insgesamt wurden mittlerweile {fancynum(vacData.administeredVaccinations)} Dosen verabreicht.</p>
       </div>
       <div className="info">
         <p>
