a8ddb7646d2f9a1ecff8cbeacd2b9f9cf1d944f1
Laurenz Grote <laurenzfg@laurenzgrote.de>
2021-05-21 16:13:08 +0200

    Simple Charts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import PieChart from './SimplePieChart'
 
 const apiEndpoint = "https://api.vacstats.laurenzfg.com/vaccinations"
 
@@ -8,7 +9,7 @@ function App() {
   const [showPushUnsubscribe, setShowUnsubscribe] = useState(false);
 
   const [lu, setLu] = useState(new Date(0)); // Last Update
-
+  
   const [vacData, setVacData] = useState({
       administeredVaccinations: 0,
       vaccinated: 0, // at least first vaccination
@@ -27,6 +28,43 @@ function App() {
         quote: 0, // percentage fully vaccination
       }
   });
+
+  // colors BioNTech, Moderna, AstraZeneca, Janssen, CureVac
+  const vacMakeColors = ['#7de01f', '#0398fc', '#ffd000', '#d60000', 'd67d00'];
+  const makeVaccineChart = () => {
+    return (
+      [
+        { name: 'BioNTech', value: vacData.vaccination.biontech },
+        { name: 'Moderna', value: vacData.vaccination.moderna },
+        { name: 'AstraZeneca', value: vacData.vaccination.astraZeneca },
+        { name: 'Janssen', value: vacData.vaccination.janssen },
+        { name: 'CureVac', value: 0 },
+      ]
+    )
+  };
+
+  // colors vac, single vac, non vac
+  const popuColors = ['#00FF08', '#EBEB34', '#EB3440'];
+  const makePopuChart = () => {
+    return (
+      [
+        { name: 'geimpft', value: vacData.secondVaccination.vaccinated },
+        { name: 'offene Impfserie', value: vacData.vaccinated - vacData.secondVaccination.vaccinated },
+        { name: 'ungeimpft', value: 83166711 - vacData.vaccinated }, // https://www.destatis.de/DE/Themen/Gesellschaft-Umwelt/Bevoelkerung/Bevoelkerungsstand/Tabellen/bevoelkerung-nichtdeutsch-laender.html
+      ]
+    );
+  };
+
+  // first jab (defined as Astra, Cure or Bion) OR second jab of prelisted + one-shot vacs
+  const firstsecColors = ['#9DB5B2', '#47AEA5'];
+  const makefirstsecChart = () => {
+    return (
+      [
+        { name: 'Erstimpfungen', value: vacData.delta },
+        { name: 'Zweitimpfungen', value: vacData.secondVaccination.delta },
+      ]
+    );
+  }
 
   useEffect(() => {
     // Update the document title using the browser API
@@ -103,6 +141,14 @@ function App() {
         {fancynum(vacData.vaccination.moderna)} Moderna, {fancynum(vacData.vaccination.astraZeneca)} AstraZeneca und {fancynum(vacData.vaccination.janssen)} Janssen.</p>
         <p>Insgesamt wurden mittlerweile {fancynum(vacData.administeredVaccinations)} Dosen verabreicht.</p>
       </div>
+      { lu > 0 &&
+        <>
+          <PieChart data={makeVaccineChart()} colors={vacMakeColors} />
+          <PieChart data={makePopuChart()} colors={popuColors} />
+          <PieChart data={makefirstsecChart()} colors={firstsecColors} />
+        </>
+      }
+  
       <div className="info">
         <div className="onesignal-customlink-container"></div>
       </div>
---
+++ src/SimplePieChart.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+export default function SimplePieChart (props) {
+
+  return (
+      <PieChart width={400} height={400}>
+        <Pie
+          data={props.data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {props.data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={props.colors[index % props.colors.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+  );
+
+}
